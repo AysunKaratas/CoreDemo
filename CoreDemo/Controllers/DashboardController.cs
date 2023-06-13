cdafd8d7b6c0f379df4a77606dab77dd44ca0431
AysunKaratas <aysun.karatas.1997@gmail.com>
2023-06-13 17:18:01 +0300

    Dashboard İstatistikleri

--- CoreDemo/Controllers/DashboardController.cs
+++ CoreDemo/Controllers/DashboardController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -12,6 +13,10 @@ namespace CoreDemo.Controllers
         [AllowAnonymous]
         public IActionResult Index()
         {
+            Context c = new Context();
+            ViewBag.ToplamBlogSayisi = c.Blogs.Count().ToString();
+            ViewBag.YazarinBlogSayisi = c.Blogs.Where(x => x.WriterID == 1).Count();
+            ViewBag.KategoriSayisi = c.Categories.Count().ToString();
             return View();
         }
     }
